d24b278b4a98acf0f3fc3d9d4a6a58b1cc50a6be
joe-rugezo <118993541+joe-rugezo@users.noreply.github.com>
2022-12-11 19:07:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+game.splash("let us calculate the area of the trapezoid")
+let length = game.askForNumber("what is the length")
+let width = game.askForNumber("what is the width")
+let height = game.askForNumber("what is the height")
+let area = length + width
+area = 30 / 2
+area = 15 * height
+game.splash("The area of a trapezoid with length" + length + "cm, width" + width + "cm and height" + height + "cm is" + area + "cm^2")
